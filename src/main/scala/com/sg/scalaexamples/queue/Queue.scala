46f6e2eadf363761c9b7b1b62508844d99b75c62
sgrishchenko <3CEBIDLZDByN>
2018-09-23 00:12:19 +0300

    add queue

---
+++ src/main/scala/com/sg/scalaexamples/queue/Queue.scala
@@ -0,0 +1,34 @@
+package com.sg.scalaexamples.queue
+
+trait Queue[+T] {
+  def head: T
+  def tail: Queue[T]
+  def enqueue[U >: T](x: U): Queue[U]
+}
+
+object Queue {
+  def apply[T](xs: T*): Queue[T] =
+    new QueueImpl[T](xs.toList, Nil)
+
+  private class QueueImpl[+T](
+    private val leading: List[T],
+    private val trailing: List[T]
+  ) extends Queue[T] {
+
+    private def mirror =
+      if (leading.isEmpty)
+        new QueueImpl(trailing.reverse, Nil)
+      else
+        this
+
+    def head: T = mirror.leading.head
+
+    def tail: Queue[T] = {
+      val q = mirror
+      new QueueImpl(q.leading.tail, q.trailing)
+    }
+
+    def enqueue[U >: T](x: U) =
+      new QueueImpl(leading, x :: trailing)
+  }
+}

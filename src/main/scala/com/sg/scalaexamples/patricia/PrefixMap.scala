d5046b97af3088572b17d60e19170ed8c7d0284a
sgrishchenko <3CEBIDLZDByN>
2018-09-27 23:57:57 +0300

    add custom sequence and map

---
+++ src/main/scala/com/sg/scalaexamples/patricia/PrefixMap.scala
@@ -0,0 +1,90 @@
+package com.sg.scalaexamples.patricia
+
+import scala.collection.generic.CanBuildFrom
+import scala.collection.mutable
+import scala.collection.immutable
+
+class PrefixMap[T]
+  extends mutable.Map[String, T]
+    with mutable.MapLike[String, T, PrefixMap[T]] {
+
+  var suffixes: immutable.Map[Char, PrefixMap[T]] = Map.empty
+  var value: Option[T] = None
+
+  def get(s: String): Option[T] =
+    if (s.isEmpty) value
+    else suffixes
+      .get(s(0))
+      .flatMap(_.get(s substring 1))
+
+  def withPrefix(s: String): PrefixMap[T] =
+    if (s.isEmpty) this
+    else {
+      val leading = s(0)
+      suffixes get leading match {
+        case None =>
+          suffixes = suffixes + (leading -> empty)
+        case _ =>
+      }
+      suffixes(leading) withPrefix (s substring 1)
+    }
+
+  override def update(s: String, elem: T): Unit =
+    withPrefix(s).value = Some(elem)
+
+  override def remove(s: String): Option[T] =
+    if (s.isEmpty) {
+      val prev = value
+      value = None
+      prev
+    }
+    else suffixes
+      .get(s(0))
+      .flatMap(_.remove(s substring 1))
+
+  def iterator: Iterator[(String, T)] = {
+    val head = for (v <- value.iterator)
+      yield ("", v)
+
+    val tail = for {
+      (chr, m) <- suffixes.iterator
+      (s, v) <- m.iterator
+    } yield (chr +: s, v)
+
+    head ++ tail
+  }
+
+  def +=(pair: (String, T)): this.type = {
+    update(pair._1, pair._2)
+    this
+  }
+
+  def -=(s: String): this.type = {
+    remove(s)
+    this
+  }
+
+  override def empty: PrefixMap[T] = PrefixMap.empty
+}
+
+object PrefixMap {
+  def empty[T] = new PrefixMap[T]
+
+  def apply[T](pairs: (String, T)*): PrefixMap[T] = {
+    val map: PrefixMap[T] = empty
+    for (pair <- pairs) map += pair
+    map
+  }
+
+  def newBuilder[T]: mutable.Builder[(String, T), PrefixMap[T]] =
+    new mutable.MapBuilder[String, T, PrefixMap[T]](empty)
+
+  implicit def canBuildFrom[T]: CanBuildFrom[PrefixMap[_], (String, T), PrefixMap[T]] =
+    new CanBuildFrom[PrefixMap[_], (String, T),
+      PrefixMap[T]] {
+      def apply(from: PrefixMap[_]): mutable.Builder[(String, T), PrefixMap[T]] = newBuilder[T]
+
+      def apply(): mutable.Builder[(String, T), PrefixMap[T]] = newBuilder[T]
+    }
+
+}
---
+++ src/main/scala/com/sg/scalaexamples/rna/RnaSeq.scala
@@ -0,0 +1,63 @@
+package com.sg.scalaexamples.rna
+
+import scala.collection.generic.CanBuildFrom
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.{IndexedSeqLike, mutable}
+
+final class RnaSeq private(
+  val groups: Array[Int],
+  val length: Int
+) extends IndexedSeq[Base] with IndexedSeqLike[Base, RnaSeq] {
+
+  import RnaSeq._
+
+  override def newBuilder: mutable.Builder[Base, RnaSeq] = RnaSeq.newBuilder
+
+  def apply(idx: Int): Base = {
+    if (idx < 0 || length <= idx)
+      throw new IndexOutOfBoundsException
+    Base.fromInt(groups(idx / N) >> (idx % N * S) & M)
+  }
+
+  override def foreach[U](f: Base => U): Unit = {
+    var i = 0
+    var b = 0
+    while (i < length) {
+      b = if (i % N == 0) groups(i / N) else b >>> S
+      f(Base.fromInt(b & M))
+      i += 1
+    }
+  }
+}
+
+object RnaSeq {
+  // Количество разрядов, необходимое для представления группы
+  private val S = 2
+  // Количество групп, помещающихся в Int-значение
+  private val N = 32 / S
+  // Битовая маска для изоляции группы
+  private val M = (1 << S) - 1
+
+  def fromSeq(buf: Seq[Base]): RnaSeq = {
+    val length = (buf.length + N - 1) / N
+    val groups = new Array[Int](length)
+
+    for (i <- buf.indices)
+      groups(i / N) |= Base.toInt(buf(i)) << (i % N * S)
+
+    new RnaSeq(groups, buf.length)
+  }
+
+  def apply(bases: Base*): RnaSeq = fromSeq(bases)
+
+  def newBuilder: mutable.Builder[Base, RnaSeq] =
+    new ArrayBuffer[Base] mapResult fromSeq
+
+  implicit def canBuildFrom: CanBuildFrom[RnaSeq, Base, RnaSeq] =
+    new CanBuildFrom[RnaSeq, Base, RnaSeq] {
+      def apply(): mutable.Builder[Base, RnaSeq] = newBuilder
+
+      def apply(from: RnaSeq): mutable.Builder[Base, RnaSeq] = newBuilder
+    }
+
+}

243e279c34793ea62f21837842a2d5d7002afaf8
sgrishchenko <3CEBIDLZDByN>
2018-09-29 00:39:37 +0300

    add xml example

--- build.sbt
+++ build.sbt
@@ -6,6 +6,8 @@ version := "0.1"
 
 scalaVersion := "2.12.6"
 
+libraryDependencies += "org.scala-lang.modules" %% "scala-xml" % "1.1.0"
+
 libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.1"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.1" % "test"
 
---
+++ src/main/scala/com/sg/scalaexamples/serialization/CCTherm.scala
@@ -0,0 +1,54 @@
+package com.sg.scalaexamples.serialization
+
+import scala.xml.Elem
+
+abstract class CCTherm {
+  val description: String
+  val yearMade: Int
+  val dateObtained: String
+  val bookPrice: Int
+  val purchasePrice: Int
+  val condition: Int
+
+  override def toString: String = description
+
+  def toXML: Elem =
+    <cctherm>
+      <description>
+        {description}
+      </description>
+      <yearMade>
+        {yearMade}
+      </yearMade>
+      <dateObtained>
+        {dateObtained}
+      </dateObtained>
+      <bookPrice>
+        {bookPrice}
+      </bookPrice>
+      <purchasePrice>
+        {purchasePrice}
+      </purchasePrice>
+      <condition>
+        {condition}
+      </condition>
+    </cctherm>
+}
+
+object CCTherm {
+  def fromXML(node: scala.xml.Node): CCTherm =
+    new CCTherm {
+      val description: String =
+        (node \ "description").text.trim
+      val yearMade: Int =
+        (node \ "yearMade").text.trim.toInt
+      val dateObtained: String =
+        (node \ "dateObtained").text.trim
+      val bookPrice: Int =
+        (node \ "bookPrice").text.trim.toInt
+      val purchasePrice: Int =
+        (node \ "purchasePrice").text.trim.toInt
+      val condition: Int =
+        (node \ "condition").text.trim.toInt
+    }
+}
